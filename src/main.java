6fa2b58424a7ab9259d38a398b4036eeeb3efcf4
tuanLamGit <tuanlam15@gmail.com>
2021-10-31 19:27:06 -0400

    added hello world

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello world, from TJ.");
+        System.out.println("Hello TJ, this is Tuan");
     }
 }
