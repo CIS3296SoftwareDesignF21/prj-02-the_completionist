0193bf110bd06be74a4bb6f58ecac08b35c250d2
Adam <adamlang739@gmail.com>
2021-11-01 16:40:31 -0400

    added hello world -Adam

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello world, from TJ.");
         System.out.println("Hello world, from Sameera.");
-
         System.out.println("Hello TJ, this is Tuan");
+        System.out.println("Hello world, from Adam.");
     }
 }
