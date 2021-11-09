046558c549659544912d3f337c936763ffddc622
tuanLamGit <tuanlam15@gmail.com>
2021-11-09 08:42:12 -0500

    fixed bugs around arraylist

--- src/main.java
+++ src/main.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        to_do_list = new to_do_list();
-        to_do_list.add_task("Do homework", "11/9/21", 1);
-        to_do_list.add_task("Workout", "11/9/21", 3);
-        to_do_list.add_task("Meet with project group", "11/9/21", 2);
-        to_do_list.print();
+        to_do_list toDoList = new to_do_list();
+        toDoList.add_task( new Task("do Homework", "11/9/21", 1, "true"));
+        toDoList.add_task( new Task("workout", "11/9/21", 3, "true"));
+        toDoList.add_task( new Task("meet with project group", "11/9/21", 2, "true"));
+        toDoList.print();
 
     }
 }
--- src/to_do_list.java
+++ src/to_do_list.java
@@ -3,10 +3,8 @@ import java.util.*;
 public class to_do_list {
     private ArrayList<Task> to_do_list;
 
-    public to_do_list(){ }
-
-    public to_do_list(ArrayList<Task> to_do_list){
-        this.to_do_list = to_do_list;
+    public to_do_list(){
+        this.to_do_list = new ArrayList<>();
     }
 
     public void add_task(Task task){
@@ -18,7 +16,7 @@ public class to_do_list {
 
     }
 
-    public void print(ArrayList<Task> to_do_list){
+    public void print(){
         for(int i = 0; i < to_do_list.size(); i++){
             System.out.println(to_do_list.get(i).toString());
             System.out.println();
