cd0a124d26694cd0900dc51beb6788dfd7687ce4
Adam <adamlang739@gmail.com>
2021-11-08 21:33:24 -0500

    added print and toString methods

--- src/main.java
+++ src/main.java
@@ -1,8 +1,6 @@
+import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello world, from TJ.");
-        System.out.println("Hello world, from Sameera.");
-        System.out.println("Hello TJ, this is Tuan");
-        System.out.println("Hello world, from Adam.");
+
     }
 }
--- src/to_do_list.java
+++ src/to_do_list.java
@@ -15,6 +15,14 @@ public class to_do_list {
 
     public void remove_task(Task task){
         to_do_list.remove(task);
+
+    }
+
+    public void print(ArrayList<Task> to_do_list){
+        for(int i = 0; i < to_do_list.size(); i++){
+            System.out.println(to_do_list.get(i).toString());
+            System.out.println();
+        }
     }
 
 }
