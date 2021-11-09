b4826c7603a36672e449717489b6a14c32373d9e
Sameera Rachakonda <srachakonda6@gmail.com>
2021-11-09 07:43:28 -0500

    Added to main.java to print out to do list

--- src/main.java
+++ src/main.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
+        to_do_list = new to_do_list();
+        to_do_list.add_task("Do homework", "11/9/21", 1);
+        to_do_list.add_task("Workout", "11/9/21", 3);
+        to_do_list.add_task("Meet with project group", "11/9/21", 2);
+        to_do_list.print();
 
     }
 }
