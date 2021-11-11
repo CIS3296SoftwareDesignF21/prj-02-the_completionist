3b63fcf16bc4a93095da7e82f87b36d7f5178f4f
tuanLamGit <tuanlam15@gmail.com>
2021-11-11 15:52:46 -0500

    reading from files and parsing works

--- src/Calendar.java
+++ src/Calendar.java
@@ -38,82 +38,13 @@ public class Calendar {
         String flag = myScanner.nextLine();
         
         writeToFile(taskName, dueDate, priority, flag);
-        //saveToArray(taskName, dueDate, priority, flag);
         //test[1].add( new Task(taskName, dueDate, Integer.parseInt(priority), flag));
        
-
         myScanner.close();
 
         return 1;
     }
-    /*
-    private void saveToArray(String taskName, String dueDate, String priority, String flag){
-
-        String month = monthFinder(dueDate);
-        int date = dateFinder(dueDate);
-
-        if(month.equals("jan")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            jan[date] = newTask;
-            
-        }
-        else if(month.equals("feb")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            feb[date] = newTask;
-    
-        }
-        else if(month.equals("mar")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            mar[date] = newTask;
-    
-        }
-        else if(month.equals("april")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            april[date] = newTask;
-        }
-        else if(month.equals("may")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            may[date] = newTask;
-    
-        }
-        else if(month.equals("june")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            june[date] = newTask;
-    
-        }
-        else if(month.equals("jul")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            jul[date] = newTask;
-    
-        }
-        else if(month.equals("aug")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            aug[date] = newTask;
-    
-        }
-        else if(month.equals("sept")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            sept[date] = newTask;
-    
-        }
-        else if(month.equals("oct")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            oct[date] = newTask;
-    
-        }
-        else if(month.equals("nov")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            nov[date] = newTask;
-    
-        }
-        else if(month.equals("dec")){
-            Task newTask = new Task(taskName, dueDate, Integer.parseInt(priority), flag);
-            dec[date] = newTask;
-    
-        }
-    }
-    */
-
+ 
     private int dateFinder(String input){
         String[] parsedString = input.split("/");
         return Integer.parseInt(parsedString[1]);
@@ -138,19 +69,6 @@ public class Calendar {
 
         try {
             FileWriter myWriter = new FileWriter( month + ".txt", true);
-//            for (int i = 1; i < 31; i++) {
-//                if (date != i) {
-//                    myWriter.write(i + " :\n");
-//                } else {
-//                    myWriter.write(i + " :\n");
-//                    myWriter.write("{\nName: " + taskName + "\n");
-//                    myWriter.write("Priority: " + priority + "\n");
-//                    myWriter.write("Flag: " + flag + "\n");
-//                    myWriter.write("}\n");
-//                }
-//            }
-//            myWriter.close();
-
             myWriter.write("\n{\nName: " + taskName + "\n");
             myWriter.write("Date: " + dueDate + "\n");
             myWriter.write("Priority: " + priority + "\n");
@@ -177,24 +95,33 @@ public class Calendar {
             while(myScanner.hasNextLine()){
                 
                 String line = myScanner.nextLine();
-                if(line.equals("{\n") || line.equals("}\n")){
+                System.out.println("Current Line: "  + line);
+                
+                if(line.equals("{") || line.equals("}")){
+
                     continue;
                 }
                 else{
+                    System.out.println("Inside else statement");
                     String[] stringArr = line.split(":");
+
                     if(stringArr[0].equals("Name")){
+                        System.out.println(stringArr[1]);
                         taskName = stringArr[1];
                         continue;
                     }
                     else if(stringArr[0].equals("Date")){
+                        System.out.println(stringArr[1]);
                         dueDate = stringArr[1];
                         continue;
                     }
                     else if(stringArr[0].equals("Priority")){
+                        System.out.println(stringArr[1]);
                         priority = stringArr[1];
                         continue;
                     }
                     else {
+                        System.out.println(stringArr[1]);
                         flag = stringArr[1];
                         continue;
                     }
@@ -202,13 +129,14 @@ public class Calendar {
                 }
             }
             int date = dateFinder(dueDate);
+            myScanner.close();
             //jan[date].add( new Task(taskName, dueDate, Integer.parseInt(priority), flag));
         }
         catch (FileNotFoundException e) {
             System.out.println("Error");
             e.printStackTrace();
         }
-      
+      /*
         try {
             File fileObj = new File("feb.txt");
             Scanner myScanner = new Scanner(fileObj);
@@ -245,6 +173,7 @@ public class Calendar {
             System.out.println("Error");
             e.printStackTrace();
         }
+        */
     }
     
     //This will convert the numerical form the month to word form of the month, to find the correct file.
