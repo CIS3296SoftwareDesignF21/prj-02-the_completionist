1018a5244cdd5bd53a6f0af5bbd48790cfea7bb4
tuanLamGit <tuanlam15@gmail.com>
2021-11-12 12:48:24 -0500

    added front end code for adding task. Need to call backend code to save user entered information

--- javafx/frontEnd/src/addTaskFrame.java
+++ javafx/frontEnd/src/addTaskFrame.java
@@ -1,7 +1,4 @@
-import java.net.SecureCacheResponse;
-
 import javax.swing.plaf.InsetsUIResource;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -9,6 +6,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
  
@@ -23,23 +23,43 @@ public class addTaskFrame extends Application {
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25,25,25,25));
-        // Button btn = new Button();
-        // btn.setText("Say 'Hello World'");
-        // btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-        //     @Override
-        //     public void handle(ActionEvent event) {
-        //         System.out.println("Hello World!");
-        //     }
-        // });
-        
-        // StackPane root = new StackPane();
-        // root.getChildren().add(btn);
 
+        Label taskName = new Label("Task Name: ");
+        grid.add(taskName, 0, 1);
+        TextField taskNameField = new TextField();
+        grid.add(taskNameField, 1, 1);
+
+        Label dateLabel = new Label("Date: ");
+        grid.add(dateLabel, 0, 2);
+        DatePicker date = new DatePicker();
+        grid.add(date, 1, 2);
+
+        Label priorityLabel = new Label("Priority: ");
+        grid.add(priorityLabel, 0, 3);
+        TextField priorityField = new TextField();
+        grid.add(priorityField, 1, 3);
+
+        Label taskTagLabel = new Label("Task Tag: ");
+        grid.add(taskTagLabel, 0, 4);
+        TextField taskTagField = new TextField();
+        grid.add(taskTagField, 1, 4);
+
+        Button btn = new Button();
+        btn.setText("Add Task");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+        @Override
+             public void handle(ActionEvent event) {
+                //add code to save task in both to-do and calendar.
+                 System.out.println("Task Added");
+             }
+        });
+        
+        grid.add(btn, 0, 6);
         Scene scene = new Scene(grid, 300, 250);
 
         primaryStage.setScene(scene);
-         primaryStage.show();
+        primaryStage.show();
     }
  public static void main(String[] args) {
         launch(args);
