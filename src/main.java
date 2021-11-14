bb6c572895c6aa91d7a88c27d0d1b84ecd7c5339
AndrewPari <tuk74485@temple.edu>
2021-11-14 17:28:26 -0500

    made buttons opaque and fit to calendar

--- src/main.java
+++ src/main.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import javax.swing.event.*;
 import java.awt.event.*;
 import javax.swing.table.*;
+import javax.swing.JLayeredPane;
 
 public class main
 {
@@ -18,15 +19,14 @@ public class main
     static JTable tableCalendar;
     static int actualDay, actualMonth, actualYear, currentMonth, currentYear;
     
-    static JButton buttonDays[] = new JButton[31];
+    static JButton buttonDays[] = new JButton[42];
     
     public static void main(String args[])
     {
         //prep frame
         frameMain = new JFrame("The Completionist");
-        frameMain.setSize(500, 500);
+        frameMain.setSize(750, 750);
         pane = frameMain.getContentPane();
-        pane.setLayout(null);
         
         //navigation
         labelMonth = new JLabel("January");
@@ -49,17 +49,29 @@ public class main
         panelCalendar = new JPanel(null);
         
         //set boundary
-        panelCalendar.setBounds(0, 0, 320, 335);
-        labelMonth.setBounds(160-labelMonth.getPreferredSize().width/2, 25, 100, 25);
-        labelYear.setBounds(10, 305, 80, 20);
-        comboYear.setBounds(230, 305, 80, 20);
+        panelCalendar.setBounds(0, 0, 335, 335);
+        labelMonth.setBounds(160-labelMonth.getPreferredSize().width/4, 25, 100, 25);
+        labelYear.setBounds(10, 450, 80, 20);
+        comboYear.setBounds(230, 450, 80, 20);
         buttonNext.setBounds(260, 25, 50, 25);
         buttonPrev.setBounds(10, 25, 50, 25);
-        stableCalendar.setBounds(10, 50, 300, 250);
+        stableCalendar.setBounds(10, 50, 400, 400);
         frameMain.setVisible(true);
         
         //add controls to screen
         pane.add(panelCalendar);
+        
+        //this is done here so the buttons are overlaid on the calendar instead of behind
+        for(int i = 0; i < 42; i++){
+            //buttonDays[i] = new JButton(Integer.toString(i));
+            buttonDays[i] = new JButton("   ");
+            //buttonDays[i].setBounds(200, 10*i, 55, 25);//x coord,y coord, x size, y size
+            panelCalendar.add(buttonDays[i]);
+            buttonDays[i].setEnabled(true);
+            buttonDays[i].setContentAreaFilled(false);//makes the button see-through
+        }
+        
+        
         panelCalendar.add(stableCalendar);
         panelCalendar.add(comboYear);
         panelCalendar.add(labelMonth);
@@ -93,9 +105,9 @@ public class main
         tableCalendar.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         
         //set row & column count
-        tableCalendar.setRowHeight(38);
-        tableCalendarModel.setColumnCount(7);
+        tableCalendar.setRowHeight(50);
         tableCalendarModel.setRowCount(6);
+        tableCalendarModel.setColumnCount(7);
         
         //fill calendar year choices, +/- 10 years from current date
         for(int i=actualYear-10; i<=actualYear+10; i++)
@@ -108,68 +120,10 @@ public class main
     }
     
     
-    public static void refreshCalendar(int month, int year)
-    {
-        int numDays, monthStart;
-        
-        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-        
-        buttonPrev.setEnabled(true);
-        buttonNext.setEnabled(true);
-        
-        labelMonth.setText(months[month]);
-        labelMonth.setBounds(160-labelMonth.getPreferredSize().width/2, 25, 180, 25);
-        comboYear.setSelectedItem(String.valueOf(year));
-        
-        for(int i = 0; i < 6; i++)
-        {
-            for(int j = 0; j < 7; j++)
-            {
-                tableCalendarModel.setValueAt(null, i, j);
-            }
-        }
-        
-        //get number of days & first day of month
-        GregorianCalendar cal = new GregorianCalendar(year, month, 1);
-        numDays = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
-        monthStart = cal.get(GregorianCalendar.DAY_OF_WEEK);
-        
-        //draw days of the month, 1-31 onto gui
-        for(int i = 1; i <= numDays; i++)
-        {
-            int row = new Integer((i + monthStart - 2) / 7);
-            int column = (i + monthStart - 2) % 7;
-            tableCalendarModel.setValueAt(i, row, column);
-            buttonDays[i] = new JButton("HERE");
-            buttonDays[i].setBounds(300,10*i,15,15);
-            panelCalendar.add(buttonDays[i]);
-        }
-        tableCalendar.setDefaultRenderer(tableCalendar.getColumnClass(0), new tableCalendarRenderer());
-    }
     
-    
-    static class tableCalendarRenderer extends DefaultTableCellRenderer
-    {
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean selected, boolean focused, int row, int column){
-            super.getTableCellRendererComponent(table, value, selected, focused, row, column);
-            
-            //default background color
-            setBackground(new Color(255, 255, 255));
-            
-            //unique color for todays date
-            if( (value != null) && ( (Integer.parseInt(value.toString()) == actualDay) && (currentMonth == actualMonth) && (currentYear == actualYear) ) )
-                setBackground(new Color(200, 200, 255));
-                
-            setBorder(null);
-            setForeground(Color.black);
-            return this;
-        }
-    }
-    
-
     static class buttonNext_Action implements ActionListener
     {
-        public void actionPerformed(ActionEvent e)
+        public void actionPerformed (ActionEvent e)
         {
             //go forward one year checkj
             if (currentMonth == 11){
@@ -198,6 +152,67 @@ public class main
                 currentMonth -= 1;
             }
             refreshCalendar(currentMonth, currentYear);
+        }
+    }
+    
+    
+    public static void refreshCalendar(int month, int year)
+    {
+        int numDays, monthStart;
+        
+        String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        
+        buttonNext.setEnabled(true);
+        buttonPrev.setEnabled(true);
+        
+        labelMonth.setText(months[month]);
+        labelMonth.setBounds(160-labelMonth.getPreferredSize().width/4, 25, 180, 25);
+        comboYear.setSelectedItem(String.valueOf(year));
+        
+        for(int i = 0; i < 6; i++)
+        {
+            for(int j = 0; j < 7; j++)
+            {
+                tableCalendarModel.setValueAt(null, i, j);
+            }
+        }
+        
+        //get number of days & first day of month
+        GregorianCalendar cal = new GregorianCalendar(year, month, 1);
+        numDays = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+        monthStart = cal.get(GregorianCalendar.DAY_OF_WEEK);
+        
+        int row, column, xCoord, yCoord = 90;
+        //draw days of the month, 1-31 onto gui
+        for(int i = 1; i <= numDays; i++)
+        {
+            if ((i-1) % 7 == 0)
+                yCoord += 55;
+            xCoord = ((i-1) % 7) * 50 + 8*((i-1)%7) + 10;
+            row = new Integer((i + monthStart - 2) / 7);
+            column = (i + monthStart - 2) % 7;
+            tableCalendarModel.setValueAt(i, row, column);
+            buttonDays[i].setBounds(xCoord, yCoord, 55, 25);//x coord,y coord, x size, y size
+        }
+        tableCalendar.setDefaultRenderer(tableCalendar.getColumnClass(0), new tableCalendarRenderer());
+    }
+    
+    
+    static class tableCalendarRenderer extends DefaultTableCellRenderer
+    {
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean selected, boolean focused, int row, int column){
+            super.getTableCellRendererComponent(table, value, selected, focused, row, column);
+            
+            //default background color
+            setBackground(new Color(255, 255, 255));
+            
+            //unique color for todays date
+            if( (value != null) && ( (Integer.parseInt(value.toString()) == actualDay) && (currentMonth == actualMonth) && (currentYear == actualYear) ) )
+                setBackground(new Color(200, 200, 255));
+                
+            setBorder(null);
+            setForeground(Color.black);
+            return this;
         }
     }
     
