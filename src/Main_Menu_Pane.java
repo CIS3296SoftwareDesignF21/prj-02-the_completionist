aab1cab2cf8e393f019acf6c6658759cd254af92
Sameera Rachakonda <srachakonda6@gmail.com>
2021-11-29 18:07:47 -0500

    Update Main_Menu_Pane.java

--- src/Main_Menu_Pane.java
+++ src/Main_Menu_Pane.java
@@ -1,29 +1,157 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.scene.text.Text;
-import javafx.scene.control.CheckBox;
 
-import java.io.FileNotFoundException;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.TimeZone;
 
 public class Main_Menu_Pane extends Application  {
 
+    Calendar cal = Calendar.getInstance(TimeZone.getDefault());
+
+    static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+    static LocalDateTime now = LocalDateTime.now();
+
+    ArrayList<Task> display = cal.readArray(dtf.format(now));
+    //cal.readArray();
+
+    public static ArrayList<Task> testarr = new ArrayList<Task>();
+    public static ArrayList<Task> testarr2 = new ArrayList<Task>();
+    public static ArrayList<Task> sevenDays = new ArrayList<Task>();
+
+    private TableView<Task> dayTable = new TableView<Task>();
+    private final ObservableList<Task> dayData =
+
+            FXCollections.observableArrayList(
+                    testarr
+            );
+
+    private TableView<Task> weekTable = new TableView<Task>();
+    private final ObservableList<Task> weekData =
+
+            FXCollections.observableArrayList(
+                    sevenDays
+            );
+
+
+    public static void main(String[] args) {
+        testarr.add(new Task("Do homework", "11/29/21", "High", "School"));
+        testarr.add(new Task("Walk dog", "11/29/21", "medium", "personal"));
+        testarr.add(new Task("eat food", "11/30/21", "low", "personal"));
+        testarr.add(new Task("eat food", "11/31/21", "low", "personal"));
+        testarr.add(new Task("eat food", "12/01/21", "low", "personal"));
+
+        int[] dayarr = new int[3];
+        String date = dtf.format(now);
+        String[] stringarr = date.split("/");
+        for(int k = 0; k < dayarr.length; k++){
+            dayarr[k] = Integer.valueOf(stringarr[k]);
+            System.out.println(dayarr[k]);
+        }
+
+        int i = 0;
+        int day = dayarr[1];
+        while(i < 7){
+            if(day > 32){
+                break;
+            }
+            else{
+                sevenDays.add(testarr.get(i));
+            }
+            i++;
+            day++;
+        }
+
+        launch(args);
+
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //Calendar cal = new Calendar();
+        Scene scene = new Scene(new Group());
+
         primaryStage.setTitle("Home Menu");
+        primaryStage.setWidth(1350);
+        primaryStage.setHeight(700);
+
+        Label dayLabel = new Label("To Do Today");
+        dayLabel.setFont(new Font("Arial", 20));
+
+        Label weekLabel = new Label("To Do In The Next 7 Days Of Current Month");
+        weekLabel.setFont(new Font("Arial", 20));
+
+        dayTable.setEditable(true);
+        weekTable.setEditable(true);
+
+        TableColumn taskCol = new TableColumn("Task");
+        taskCol.setMinWidth(100);
+        taskCol.setCellValueFactory(
+                new PropertyValueFactory<Task, String>("name"));
+
+        TableColumn dateCol = new TableColumn("Date");
+        dateCol.setMinWidth(100);
+        dateCol.setCellValueFactory(
+                new PropertyValueFactory<Task, String>("date"));
+
+        TableColumn priorityCol = new TableColumn("Priority");
+        priorityCol.setMinWidth(200);
+        priorityCol.setCellValueFactory(
+                new PropertyValueFactory<Task, String>("priority"));
+
+        TableColumn tagCol = new TableColumn("Tag");
+        tagCol.setMinWidth(200);
+        tagCol.setCellValueFactory(
+                new PropertyValueFactory<Task, String>("tag"));
+
+        dayTable.setItems(dayData);
+        dayTable.getColumns().addAll(taskCol, dateCol, priorityCol, tagCol);
+
+        TableColumn weekTask = new TableColumn("Task");
+        weekTask.setMinWidth(100);
+        weekTask.setCellValueFactory(
+                new PropertyValueFactory<Task, String>("name"));
+
+        TableColumn weekDate = new TableColumn("Date");
+        weekDate.setMinWidth(100);
+        weekDate.setCellValueFactory(
+                new PropertyValueFactory<Task, String>("date"));
+
+        TableColumn weekPriority = new TableColumn("Priority");
+        weekPriority.setMinWidth(200);
+        weekPriority.setCellValueFactory(
+                new PropertyValueFactory<Task, String>("priority"));
+
+        TableColumn weekTag = new TableColumn("Tag");
+        weekTag.setMinWidth(200);
+        weekTag.setCellValueFactory(
+                new PropertyValueFactory<Task, String>("tag"));
+
+        weekTable.setItems(weekData);
+        weekTable.getColumns().addAll(weekTask, weekDate, weekPriority, weekTag);
 
         Button button1 = new Button("Calendar");
-        CalendarPane cal = new CalendarPane();
+        /*CalendarPane cal = new CalendarPane();
         button1.setOnAction(e -> {
             try {
                 cal.main();
             } catch (FileNotFoundException ex) {
                 ex.printStackTrace();
             }
-        });
+        });*/
 
         Button button2 = new Button("Add Task");
         Add_Task_Pane add = new Add_Task_Pane();
@@ -51,106 +179,28 @@ public class Main_Menu_Pane extends Application  {
             }
         });
 
-        Button button5 = new Button("Feature 3");
+        //System.out.println(dtf.format(now));
+        //System.out.println(display.toString());
 
-        Text textToDoToday = new Text("To Do Today");
+        HBox hboxLabels = new HBox();
+        hboxLabels.getChildren().addAll(dayLabel, weekLabel);
+        hboxLabels.setSpacing(500);
+        hboxLabels.setAlignment(Pos.TOP_CENTER);
 
-        Text textTask = new Text("Task");
-        Text textTask1 = new Text("Task 1");
-        Text textTask2 = new Text("Task 2");
-        Text textTask3 = new Text("Task 3");
+        HBox hboxData = new HBox();
+        hboxData.getChildren().addAll(dayTable, weekTable);
+        hboxData.setSpacing(100);
 
-        Text textWeekTask = new Text("Task");
-        Text textWeekTask1 = new Text("Task 1");
-        Text textWeekTask2 = new Text("Task 2");
-        Text textWeekTask3 = new Text("Task 3");
+        HBox hboxButtons = new HBox();
+        hboxButtons.getChildren().addAll(button1,button2,button3,button4);
+        hboxButtons.setSpacing(300);
+        hboxButtons.setAlignment(Pos.BOTTOM_CENTER);
 
-        Text textPriority = new Text("Priority");
-        Text textHighPriority = new Text("High");
-        Text textMediumPriority = new Text("Medium");
-        Text textLowPriority = new Text("Low");
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(hboxLabels, hboxData, hboxButtons);
 
-        Text textWeekPriority = new Text("Priority");
-        Text textWeekHighPriority = new Text("High");
-        Text textWeekMediumPriority = new Text("Medium");
-        Text textWeekLowPriority = new Text("Low");
+        ((Group) scene.getRoot()).getChildren().addAll(vbox);
 
-        Text textTag = new Text("Tag");
-        Text textWorkTag = new Text("Work");
-        Text textSchoolTag = new Text("School");
-        Text textPersonalTag = new Text("Personal");
-
-        Text textWeekTag = new Text("Tag");
-        Text textWeekWorkTag = new Text("Work");
-        Text textWeekSchoolTag = new Text("School");
-        Text textWeekPersonalTag = new Text("Personal");
-
-        Text textDone = new Text("Done");
-
-        CheckBox checkBox1 = new CheckBox("Completed");
-        CheckBox checkBox2 = new CheckBox("Completed");
-        CheckBox checkBox3 = new CheckBox("Completed");
-
-        Text textWeekOverview = new Text("Next 7 Days");
-        Text textDueDate = new Text("Due Date");
-        Text textDueDate1 = new Text("01/01/2022");
-        Text textDueDate2 = new Text("01/02/2022");
-        Text textDueDate3 = new Text("01/03/2022");
-
-        GridPane gridPane = new GridPane();
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
-
-        gridPane.add(button1, 0, 0, 1, 1);
-        gridPane.add(button2, 2, 0, 1, 1);
-        gridPane.add(button3, 0, 16, 1, 1);
-        gridPane.add(button4, 2, 16, 1, 1);
-        gridPane.add(button5, 4, 16, 1, 1);
-
-        gridPane.add(textToDoToday, 1, 2, 1, 1);
-
-        gridPane.add(textTask, 0,3,1,1);
-        gridPane.add(textTask1, 0, 4,1,1);
-        gridPane.add(textTask2, 0,5,1,1);
-        gridPane.add(textTask3,0,6,1,1);
-
-        gridPane.add(textPriority,1,3,1,1);
-        gridPane.add(textHighPriority,1,4,1,1);
-        gridPane.add(textMediumPriority,1,5,1,1);
-        gridPane.add(textLowPriority,1,6,1,1);
-
-        gridPane.add(textTag,2,3,1,1);
-        gridPane.add(textWorkTag,2,4,1,1);
-        gridPane.add(textSchoolTag,2,5,1,1);
-        gridPane.add(textPersonalTag,2,6,1,1);
-
-        gridPane.add(textDone,3,3,1,1);
-        gridPane.add(checkBox1,3,4,1,1);
-        gridPane.add(checkBox2,3,5,1,1);
-        gridPane.add(checkBox3,3,6,1,1);
-
-        gridPane.add(textWeekOverview,1,10,1,1);
-        gridPane.add(textWeekTask,0,11,1,1);
-        gridPane.add(textWeekTask1,0,12,1,1);
-        gridPane.add(textWeekTask2,0,13,1,1);
-        gridPane.add(textWeekTask3,0,14,1,1);
-
-        gridPane.add(textWeekPriority,1,11,1,1);
-        gridPane.add(textWeekHighPriority,1,12,1,1);
-        gridPane.add(textWeekMediumPriority,1,13,1,1);
-        gridPane.add(textWeekLowPriority,1,14,1,1);
-
-        gridPane.add(textWeekTag,2,11,1,1);
-        gridPane.add(textWeekWorkTag,2,12,1,1);
-        gridPane.add(textWeekSchoolTag,2,13,1,1);
-        gridPane.add(textWeekPersonalTag,2,14,1,1);
-
-        gridPane.add(textDueDate,3,11,1,1);
-        gridPane.add(textDueDate1,3,12,1,1);
-        gridPane.add(textDueDate2,3,13,1,1);
-        gridPane.add(textDueDate3,3,14,1,1);
-
-        Scene scene = new Scene(gridPane, 500, 500);
         primaryStage.setScene(scene);
         primaryStage.show();
 
